a950f6beccf7807b5e72149a59ca34d361afcae0
Radu Pisoi <radu_pisoi@sync.ro>
2018-09-07 16:57:33 +0300

    Added FeedbackEventListener

---
+++ src/main/java/com/oxygenxml/webhelp/feedback/event/FeedbackEventType.java
@@ -0,0 +1,17 @@
+package com.oxygenxml.webhelp.feedback.event;
+
+/**
+ * Enumeration for feedback event types.
+ */
+public enum FeedbackEventType {
+	
+	/** 
+	 * A new message was added.
+	 */
+	NEW_MESSAGE_WAS_ADDED,
+	
+	/**
+	 * A message was marked as junk.
+	 */
+	MESSAGE_APPROVED
+}
---
+++ src/main/java/com/oxygenxml/webhelp/feedback/event/IFeedbackEvent.java
@@ -0,0 +1,12 @@
+package com.oxygenxml.webhelp.feedback.event;
+
+/**
+ * Interface to represent a feedback event.
+ */
+public interface IFeedbackEvent {
+	
+	/**
+	 * @return The feedback event type.
+	 */
+	FeedbackEventType getType();
+}
